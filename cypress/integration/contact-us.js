23d767fa987b436244c944d656d937d2e775c345
Nenad <petrovicnenad@gmail.com>
2021-03-15 17:17:37 +0100

    commit 2

---
+++ cypress/integration/contact-us.js
@@ -0,0 +1,12 @@
+/// <reference types="cypress"/>
+
+describe("Test Contact Us form via Webdriver", () => {
+
+    it("Should be able to submit a succesful submission via contact us form", () = > {
+    cy.visit("http://www.webdriveruniversity.com")
+    cy.get('#contact-us').click({force: true})    
+
+});
+
+
+})
