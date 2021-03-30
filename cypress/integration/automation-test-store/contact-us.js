911c570a621ec78c2879718947873c0bce434e6c
Nenad <petrovicnenad@gmail.com>
2021-03-30 14:10:16 +0200

    Commit 4

--- cypress/integration/automation-test-store/contact-us.js
+++ cypress/integration/automation-test-store/contact-us.js
@@ -4,7 +4,11 @@ describe("Test Contact Us via Automation Test Store", () => {
 
     it("Should be able to submit a succesful submission via contact us form", () => {
         cy.visit("https://automationteststore.com/");
-        
+        cy.get('.info_links_footer > :nth-child(5) > a').click();
+        cy.get('#ContactUsFrm_first_name').type("Joe");
+        cy.get('#ContactUsFrm_email').type("joe_blogs123@gmail.com");
+        cy.get('#ContactUsFrm_enquiry').type("Do you provide additional discount on bulk order")
+        cy.get('.col-md-6 > .btn').click()
     });
 
 })
